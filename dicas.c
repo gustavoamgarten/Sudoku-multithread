fc381ad5338426edde92a767004a1cbc05405981
Gustavo Amgarten de Lêdo <gustavo@duckstorm.(none)>
2014-03-16 18:33:28 -0300

    Adicionado Multithread no módulo de dicas

--- dicas.c
+++ dicas.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <pthread.h>
+#include <stdlib.h>
 #include "dicas.h"
 
 #define TAB_LARG 9
@@ -7,65 +9,190 @@
 #define MAX_CASAS 81
 #define MAX_NUM 9
 
-//ASCII = Subtrair 48
+// ##############################
+// Estruturas necessárias para passar mais de uma variável para uma função utilizada em uma thread.
+// ##############################
 
-int dicas(char tab[TAB_LARG][TAB_ALT]){
+typedef struct argPercorre {
+	int i;
+	char (*tab)[9];
+} ArgPercorre;
+
+typedef struct argPercorreQuadrado {
+	int inicioLinha;
+	int inicioColuna;
+	char (*tab)[9];
+} ArgPercorreQuadrado;
+
+typedef struct argVerificaSequencia {
+	int searchValue;
+	char *sequencia;
+} ArgVerificaSequencia;
+
+// ##############################
+// Fim estruturas
+// ##############################
+
+
+// ##############################
+// Funções chamadas pelas threads.
+// ##############################
+
+// Função para percorrer a linha do campo atual.
+void *percorre_linha(void *arg){
+	ArgPercorre argumento;
+	argumento = *(ArgPercorre *)arg;
 	
-	int i, j, k, l, m, n;
-	char dica[TAB_LARG][TAB_ALT];
+	int k = argumento.i;
+	int l;
+	char *seqLinha = (char *) malloc(9*sizeof(char));
+	char (*tab)[9] = argumento.tab;
 	
-	char possibilidades[MAX_CASAS][MAX_NUM + 1];
-	int numDicas = 0;
+	for(l = 0; l < TAB_LARG; l++){
+		seqLinha[l] = tab[k][l];
+	}
 	
-	char seqLinha[MAX_NUM];
-	char seqColuna[MAX_NUM];
-	char seqQuadrado[MAX_NUM];
-	int existeLinha = 0;
-	int existeColuna = 0;
-	int existeQuadrado = 0;
-	int maxValue = 0;
+	return seqLinha;
+}
+
+// Função para percorrer a coluna do campo atual.
+void *percorre_coluna(void *arg){
+	ArgPercorre argumento;
+	argumento = *(ArgPercorre *)arg;
 	
-	int inicioLinha, inicioColuna;
+	int k = argumento.i;
+	int l;
+	char *seqColuna = (char *) malloc(9*sizeof(char));
+	char (*tab)[9] = argumento.tab;
 	
-	for(i = 0; i < TAB_LARG; i++) {
-		for(j = 0; j < TAB_ALT; j++){
-			dica[i][j] = tab[i][j];
+	for(l = 0; l < TAB_LARG; l++){
+		seqColuna[l] = tab[l][k];
+	}
+	
+	return seqColuna;
+}
+
+// Função para percorrer o quadrado do campo atual.
+void *percorre_quadrado(void *arg){
+	ArgPercorreQuadrado argumento;
+	argumento = *(ArgPercorreQuadrado *)arg;
+	
+	int m, n, k, l;
+	char *seqQuadrado = (char *) malloc(9*sizeof(char));
+	char (*tab)[9] = argumento.tab;
+	
+	for(m = argumento.inicioLinha, k = 0; m < (argumento.inicioLinha + 3); m++, k++){
+		for(n = argumento.inicioColuna, l = 0; n < (argumento.inicioColuna + 3); n++, l++){
+			seqQuadrado[(k*3)+l] = tab[m][n];
 		}
 	}
 	
+	return seqQuadrado;
+}
+
+// Função que verifica a existencia de um numero em uma sequencia (array).
+//void *verificaExistencia(char *sequencia, int searchValue){
+void *verificaExistencia(void *arg){
+	ArgVerificaSequencia argumento = *(ArgVerificaSequencia *)arg;
+	int i;
+	int *returnValue = (int *) malloc(sizeof(int));
+	char charSearchValue = argumento.searchValue;
+	for(i = 0; i < MAX_NUM; i++){
+		if(argumento.sequencia[i] == charSearchValue){
+			*returnValue = 1;
+			return returnValue;
+		}
+	}
+	
+	*returnValue = 0;
+	return returnValue;
+}
+
+// ##############################
+// Fim das funções chamadas pelas threads.
+// ##############################
+
+// Função para procurar as dicas no tabuleiro fornecido em uma matriz char 9x9.
+int dicas(char tab[TAB_LARG][TAB_ALT]){
+	
+	int i, j, k, l, m, n; // Variáveis contadoras
+	
+	char possibilidades[MAX_CASAS][MAX_NUM + 1]; // Vetor que contém sequências de números possíveis para cada campo em que foi encontrado um X.
+	int numDicas = 0; // Contador do número de campos que requerem dicas.
+	
+	// Variáveis que armazenam o retorno das funções que verificam se algum número existe na linha, coluna ou quadrado indicado.
+	int *existeLinha;
+	int *existeColuna;
+	int *existeQuadrado;
+	
+	int maxValue = 0; // Contador que indica a quantidade de numeros possíveis em cada campo.
+	
+	int inicioLinha, inicioColuna; // Contadores que indicam o ponto de inicio de um quadrado referente a um elemento do tabuleiro.
+	
+	// Threads que serão utilizadas.
+	pthread_t thread[3];
+	
+	// Vetores que armazenam as sequencias contidas na linha, coluna e quadrado de uma posição do tabuleiro.
+	char *seqLinha;
+	char *seqColuna;
+	char *seqQuadrado;
+	
+	// Variáveis que armazenam os argumentos necessários para as funções chamadas nas threads.
+	ArgPercorre *argLinha = (ArgPercorre *) malloc(sizeof(ArgPercorre));
+	ArgPercorre *argColuna = (ArgPercorre *) malloc(sizeof(ArgPercorre));
+	ArgPercorreQuadrado *argQuadrado = (ArgPercorreQuadrado *) malloc(sizeof(ArgPercorreQuadrado)); 
+	ArgVerificaSequencia *argVerifica = (ArgVerificaSequencia *) malloc(3*sizeof(ArgVerificaSequencia)); 
+	
+	// Percorre o tabuleiro procurando os campos que contém um X
 	for(i = 0; i < TAB_LARG; i++) {
 		for(j = 0; j < TAB_ALT; j++){
 			if(tab[i][j] == 'X') {
 				
 				maxValue = 0;
 				
-				// Percorre a linha (Pode ser jogado em uma thread)
-				k = i;
-				for(l = 0; l < TAB_LARG; l++){
-					seqLinha[l] = tab[k][l];
-				}
+				// Percorre a linha em uma thread própria.
+				argLinha->i = i;
+				argLinha->tab = tab;
+				pthread_create(&(thread[0]), NULL, percorre_linha, (void *)argLinha);
 				
-				// Percorre a coluna (Pode ser jogado em uma thread)
-				l = j;
-				for(k = 0; k < TAB_LARG; k++){
-					seqColuna[k] = tab[k][l];
-				}
+				// Percorre a coluna em uma thread própria.				
+				argColuna->i = j;
+				argColuna->tab = tab;
+				pthread_create(&(thread[1]), NULL, percorre_coluna, (void *)argColuna);
 				
-				// Percorre o quadrado (Pode ser jogado em uma thread)
-				inicioLinha = ((i/3)*3);
-				inicioColuna = ((j/3)*3);
-				for(m = inicioLinha, k = 0; m < (inicioLinha + 3); m++, k++){
-					for(n = inicioColuna, l = 0; n < (inicioColuna + 3); n++, l++){
-						seqQuadrado[(k*3)+l] = tab[m][n];
-					}
-				} 
+				// Percorre o quadrado em uma thread própria.
+				argQuadrado->inicioLinha = ((i/3)*3);
+				argQuadrado->inicioColuna = ((j/3)*3);
+				argQuadrado->tab = tab;
+				pthread_create(&(thread[2]), NULL, percorre_quadrado, (void *)argQuadrado); 
+				
+				// Espera percorrer todos os campos necessarios e armazena as sequencias nas respectivas variáveis.
+				pthread_join(thread[0], (void**)&seqLinha);
+				pthread_join(thread[1], (void**)&seqColuna);
+				pthread_join(thread[2], (void**)&seqQuadrado); 
 				
 				for(k = 0; k < MAX_NUM; k++){
-					existeLinha = verificaExistencia(seqLinha, k+49);
-					existeColuna = verificaExistencia(seqColuna, k+49);
-					existeQuadrado = verificaExistencia(seqQuadrado, k+49);
+					// Verifica a existência do número k, na respectiva linha
+					argVerifica[0].searchValue = k+49;
+					argVerifica[0].sequencia = seqLinha;
+					pthread_create(&(thread[0]), NULL, verificaExistencia, (void *)&argVerifica[0]);
 					
-					if(existeLinha == 0 && existeColuna == 0 && existeQuadrado == 0){
+					// Verifica a existência do número k, na respectiva coluna
+					argVerifica[1].searchValue = k+49;
+					argVerifica[1].sequencia = seqColuna;
+					pthread_create(&(thread[1]), NULL, verificaExistencia, (void *)&argVerifica[1]);
+					
+					// Verifica a existência do número k, no respectivo quadrado
+					argVerifica[2].searchValue = k+49;
+					argVerifica[2].sequencia = seqQuadrado;
+					pthread_create(&(thread[2]), NULL, verificaExistencia, (void *)&argVerifica[2]);
+					
+					// Espera percorrer todos os campos necessarios e armazena as sequencias nas respectivas variáveis.
+					pthread_join(thread[0], (void*)&existeLinha);
+					pthread_join(thread[1], (void*)&existeColuna);
+					pthread_join(thread[2], (void*)&existeQuadrado); 
+					
+					if(*existeLinha == 0 && *existeColuna == 0 && *existeQuadrado == 0){
 						maxValue++;
 						possibilidades[numDicas][maxValue] = k+49;
 					}
@@ -79,6 +206,7 @@ int dicas(char tab[TAB_LARG][TAB_ALT]){
 		} //Fim for
 	} //Fim for
 	
+	// Percorre o tabuleiro novamente, indicando as dicas nos respectivos campos com X.
 	k = 0;
 	for(i = 0; i < TAB_ALT; i++){
 		for(j = 0; j < TAB_LARG; j++){
@@ -99,15 +227,3 @@ int dicas(char tab[TAB_LARG][TAB_ALT]){
 	return 1;
 	
 } //Fim função dicas
-
-int verificaExistencia(char *sequencia, int searchValue){
-	int i;
-	char charSearchValue = searchValue;
-	for(i = 0; i < MAX_NUM; i++){
-		if(sequencia[i] == charSearchValue){
-			return 1;
-		}
-	}
-	
-	return 0;
-}
--- makefile
+++ makefile
@@ -1,11 +1,11 @@
 sudoku : sudoku.o
-	gcc -o sudoku sudoku.o dicas.o
+	gcc -o sudoku sudoku.o dicas.o -lpthread
 	
 sudoku.o : sudoku.c dicas.o
 	gcc -fno-stack-protector -g -c sudoku.c
 	
-dicas.o : dicas.c
-	gcc -fno-stack-protector -g -c dicas.c
+dicas.o : dicas.c dicas.h
+	gcc -fno-stack-protector -pthread -g -c dicas.c
 	
 clean :
 	rm sudoku sudoku.o dicas.o
